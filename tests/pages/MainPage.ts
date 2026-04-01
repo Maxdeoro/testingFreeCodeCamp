bc92726391613b8b61a10653fb1054e66ab25f1a
Maxdeoro <maxdeoro@gmail.com>
2026-04-01 23:23:49 +0300

    check snapshots matching for heading

--- tests/pages/MainPage.ts
+++ tests/pages/MainPage.ts
@@ -1,8 +1,25 @@
-import { Page } from "playwright/test";
+import { Page, Locator, expect } from "@playwright/test";
 import { BasePage } from './BasePage';
 
 export class MainPage extends BasePage {
+    private readonly curriculumLocator: Locator;
+    private readonly headingLocator: Locator;
+
+    constructor(page: Page) {
+        super(page);
+        this.curriculumLocator = this.page.getByRole('link', {name: 'freeCodeCamp Curriculum'});
+        this.headingLocator = this.page.getByRole('heading', {name: 'Build Your Skills for Free'});
+    }
+
     async open() {
         await this.page.goto('https://www.freecodecamp.org/');
+    };
+
+    async curriculumLinkHasCorrectSnapshot() {
+        await expect(this.curriculumLocator).toMatchAriaSnapshot();
+    };
+
+    async headingHasCorrectSnapshot() {
+        await expect(this.headingLocator).toMatchAriaSnapshot();
     };
 };
--- tests/specs/signedOutUser/mainPage.spec.ts
+++ tests/specs/signedOutUser/mainPage.spec.ts
@@ -1,7 +1,19 @@
-import { test, expect } from 'playwright/test';
+import { test, expect } from '@playwright/test';
 import { MainPage } from '../../pages/MainPage';
 
 test('Open main page', async ({page}) => {
     const mainPage = new MainPage(page);
     await mainPage.open();
+});
+
+test('Check curriculum snapshot is correct', async ({page}) => {
+    const mainPage = new MainPage(page);
+    await mainPage.open();
+    await mainPage.curriculumLinkHasCorrectSnapshot();
+});
+
+test('Check heading snapshot iscorrect', async ({page}) => {
+    const mainPage = new MainPage(page);
+    await mainPage.open();
+    await mainPage.headingHasCorrectSnapshot();
 });
