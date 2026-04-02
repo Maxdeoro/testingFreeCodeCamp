04656715b119c42b8413a542b2e382849eaf036e
Maxdeoro <maxdeoro@gmail.com>
2026-04-02 22:41:43 +0300

    check regForm and curriculumPage opening

--- tests/pages/MainPage.ts
+++ tests/pages/MainPage.ts
@@ -4,11 +4,17 @@ import { BasePage } from './BasePage';
 export class MainPage extends BasePage {
     private readonly curriculumLocator: Locator;
     private readonly headingLocator: Locator;
+    private readonly recommendedCurriculumLocator: Locator;
+    private readonly startLearningButtonLocator: Locator;
+    private readonly javaScriptCertificationLocator: Locator;
 
     constructor(page: Page) {
         super(page);
         this.curriculumLocator = this.page.getByRole('link', {name: 'freeCodeCamp Curriculum'});
         this.headingLocator = this.page.getByRole('heading', {name: 'Build Your Skills for Free'});
+        this.recommendedCurriculumLocator =  this.page.locator('div').filter({hasText: 'Recommended curriculum'}).nth(4);
+        this.startLearningButtonLocator = this.page.getByRole('link', {name: "Start Learning Now (it's free)"});
+        this.javaScriptCertificationLocator = this.page.getByRole('link', {name: 'JavaScript Certification'});
     }
 
     async open() {
@@ -21,5 +27,21 @@ export class MainPage extends BasePage {
 
     async headingHasCorrectSnapshot() {
         await expect(this.headingLocator).toMatchAriaSnapshot();
+    };
+
+    async recommendedCurriculumIsVisible() {
+        await expect(this.recommendedCurriculumLocator).toBeVisible();
+    };
+
+    async startLearningNowButtonVisibility() {
+        await expect(this.startLearningButtonLocator).toBeVisible();
+    };
+
+    async openAuthForm() {
+        await this.startLearningButtonLocator.click();
+    };
+
+    async openJavaScriptPage() {
+        await this.javaScriptCertificationLocator.click();
     };
 };
--- tests/specs/signedOutUser/mainPage.spec.ts
+++ tests/specs/signedOutUser/mainPage.spec.ts
@@ -1,19 +1,25 @@
-// import { MainPage } from '../../pages/MainPage';
 import { test, expect } from '../../fixtures/fixtures';
 
-// test('Open main page', async ({page}) => {
-//     const mainPage = new MainPage(page);
-//     await mainPage.open();
-// });
-
 test('Check curriculum snapshot is correct', async ({mainPage}) => {
-    // const mainPage = new MainPage(page);
-    // await mainPage.open();
     await mainPage.curriculumLinkHasCorrectSnapshot();
 });
 
-test('Check heading snapshot iscorrect', async ({mainPage}) => {
-    // const mainPage = new MainPage(page);
-    // await mainPage.open();
+test('Check heading snapshot is correct', async ({mainPage}) => {
     await mainPage.headingHasCorrectSnapshot();
+});
+
+test('Check visibility of the recommended curriculum block', async ({mainPage}) => {
+    await mainPage.recommendedCurriculumIsVisible();
+});
+
+test('visibility of the StartLearningNowButton', async ({mainPage}) => {
+    await mainPage.startLearningNowButtonVisibility();
+});
+
+test('Check authorization form opening', async ({mainPage}) => {
+    await mainPage.openAuthForm();
+});
+
+test('Check JavaScript Certification page opening', async ({mainPage}) => {
+    await mainPage.openJavaScriptPage();
 });
