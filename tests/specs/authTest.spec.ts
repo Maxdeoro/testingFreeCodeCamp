6b7010cd9825418680ff800e2f3eb503210271f6
Maxdeoro <55924753+Maxdeoro@users.noreply.github.com>
2026-04-09 22:07:04 +0300

    Obscure email address in auth test

--- tests/specs/authTest.spec.ts
+++ tests/specs/authTest.spec.ts
@@ -13,7 +13,7 @@ test('Check Sign in with email', async ({page}) => {
 
     const emailInput = page.getByRole('textbox', {name: 'Email address'});
 
-    await emailInput.fill('maxdeoro@gmail.com');
+    await emailInput.fill('...');
 
     const continueButton = page.getByRole('button', {name: 'Continue with Email'});
     await continueButton.click();
