fade56cc824dd075ce8986ad907614209d562c27
Maxdeoro <maxdeoro@gmail.com>
2026-04-10 22:55:05 +0300

    code refacturing

--- tests/pages/BasePage.ts
+++ tests/pages/BasePage.ts
@@ -1,9 +1,17 @@
-import { Page } from '@playwright/test';
+import { expect, Locator, Page } from '@playwright/test';
 
 export class BasePage {
     readonly page: Page;
 
     constructor(page: Page) {
         this.page = page;
+    };
+
+    protected async checkAriaSnapshot(locator: Locator) {
+        await expect(locator).toMatchAriaSnapshot();
+    };
+
+    protected async checkVisibility(locator: Locator) {
+        await expect(locator).toBeVisible();
     };
 };
